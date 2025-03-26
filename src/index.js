701c15c926f9f61c23f3ea8e5f7e20c94f3981f7
mykd2002 <mykd2002@gmail.com>
2025-03-26 16:51:53 +0900

    완성파일

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';  // 변경된 부분
-import App from './App';  // App 컴포넌트 임포트
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("potato"));
